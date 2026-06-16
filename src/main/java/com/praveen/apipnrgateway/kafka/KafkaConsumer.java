7ca121878d0b2d3873207906f90f271a315f097b
DHEERENDRA <lifesstudy@gmail.com>
2026-06-16 22:24:40 +0530

    kafka group fix

--- src/main/java/com/praveen/apipnrgateway/kafka/KafkaConsumer.java
+++ src/main/java/com/praveen/apipnrgateway/kafka/KafkaConsumer.java
@@ -4,10 +4,7 @@ import com.praveen.apipnrgateway.helper.Utils;
 import com.praveen.apipnrgateway.dto.CheckInRequest;
 import com.praveen.apipnrgateway.dto.DCSRequest;
 import com.praveen.apipnrgateway.dto.PnrRequest;
-import com.praveen.apipnrgateway.kafka.events.CheckInEvent;
-import com.praveen.apipnrgateway.kafka.events.DCSRequestEvent;
-import com.praveen.apipnrgateway.kafka.events.KafkaTopics;
-import com.praveen.apipnrgateway.kafka.events.PnrEvent;
+import com.praveen.apipnrgateway.kafka.events.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -23,21 +20,21 @@ public class KafkaConsumer {
 
     private final KafkaService kafkaService;
 
-    @KafkaListener(topics = KafkaTopics.PNR_EVENTS, groupId = "group-id1")
+    @KafkaListener(topics = KafkaTopics.PNR_EVENTS, groupId = KafkaGroups.PNR_PROCESSOR_GROUP)
     public void consumingPnrRequest(@Payload String request, @Header(value = KafkaHeaders.RECEIVED_KEY) String pnrId) {
         log.info("✓ Received PnrEvent via Kafka Broker partition. PNR Key: {}, Action: {}", pnrId, request);
         PnrEvent pnrEvent = Utils.jsonToObject(request, PnrEvent.class);
         kafkaService.processPnrMessage(pnrEvent);
     }
 
-    @KafkaListener(topics = KafkaTopics.CheckIn.REQUESTS, groupId = "group-id2")
+    @KafkaListener(topics = KafkaTopics.CheckIn.REQUESTS, groupId = KafkaGroups.DCS_VALIDATION_GROUP)
     public void consumingCheckInRequest(@Payload String request, @Header(value = KafkaHeaders.RECEIVED_KEY) String pnrId) {
         log.info("✓ Received CheckInEvent via Kafka Broker partition. PNR Key: {}, Action: {}", pnrId, request);
         CheckInEvent checkInEvent = Utils.jsonToObject(request, CheckInEvent.class);
         kafkaService.processCheckInMessage(checkInEvent);
     }
 
-    @KafkaListener(topics = KafkaTopics.DCS_EVENTS, groupId = "group-id3")
+    @KafkaListener(topics = KafkaTopics.DCS_EVENTS, groupId = KafkaGroups.DCS_PROCESSOR_GROUP)
     public void consumingDCSRequest(@Payload String request, @Header(value = KafkaHeaders.RECEIVED_KEY) String pnrId) {
         log.info("✓ Received DCSRequestEvent via Kafka Broker partition. PNR Key: {}, Action: {}", pnrId, request);
         DCSRequestEvent dcsRequestEvent = Utils.jsonToObject(request, DCSRequestEvent.class);
